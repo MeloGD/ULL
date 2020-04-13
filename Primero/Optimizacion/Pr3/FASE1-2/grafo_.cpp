a0f4c87a62a4978fe2c93684a475114649d363fd
SilverGG <meloglez.96@gmail.com>
2020-04-14 00:05:31 +0100

    Opt

---
+++ Primero/Optimizacion/Pr3/FASE1-2/grafo_.cpp
@@ -0,0 +1,77 @@
+﻿ /*
+ *  GRAFO.CPP - Implementación de la clase GRAFOS
+ *
+ *
+ *  Autor : Antonio Sedeno Noda, Sergio Alonso
+ *  Fecha : 2013-2020
+ */
+
+#include "grafo.h"
+
+GRAFO::~GRAFO()
+{
+}
+
+void GRAFO:: actualizar (char nombrefichero[85], int &errorapertura)
+{
+}
+
+void GRAFO::Info_Grafo()
+{
+    if (dirigido == 1)
+    {
+        cout << "Grafo dirigido ";
+    }
+    else
+    {
+        cout << "Grafo no dirigido ";
+    };
+    cout << " | nodos " << n << " | ";
+    if (dirigido == 1)
+    {
+        cout << " arcos ";
+    }
+    else
+    {
+        cout << "aristas ";
+    };
+    cout << m << " " << endl;
+}
+
+unsigned GRAFO::Es_dirigido()
+{
+	return dirigido;
+}
+
+void Mostrar_Lista(vector<LA_nodo> L)
+{
+};
+
+void GRAFO :: Mostrar_Listas (int l)
+{
+};
+
+void GRAFO::dfs(unsigned i, vector<bool> &visitado)
+{
+	visitado[i] = true;
+    cout << i+1 << ", ";
+	for (unsigned j=0;j<LS[i].size();j++)
+             if (visitado[LS[i][j].j] == false)
+                 dfs(LS[i][j].j, visitado);
+}
+
+void GRAFO::ComponentesConexas()
+{
+}
+
+void GRAFO::ListaPredecesores() 
+{
+}
+
+GRAFO::GRAFO(char nombrefichero[85], int &errorapertura)
+{
+}
+
+
+
+
---
+++ Primero/Optimizacion/Pr3/FASE1-2/pg1_.cpp
@@ -0,0 +1,65 @@
+ /*
+ *  PG1.CPP - Actividad I de Optimiza!cion.
+ *
+ *
+ *               Autores : Antonio Sedeno Noda, Sergio Alonso.
+ *               Fecha : Marzo-2020
+ */
+
+#include <string.h>
+#include "grafo.h"
+
+void clrscr() //No funciona en todos los sistemas: opcional
+{
+	system("cls");
+}
+
+
+void menu (unsigned dirigido, char &opcion)
+//Expresion del menu de opciones segun sea un grafo dirigido o no dirigido
+{
+   
+       //Cumplimentar por alumnado según pantallas de opciones en el guión u otras equivalentes
+    
+    };
+
+
+// El principal es simplemente un gestor de menu, diferenciando acciones para grafo dirigido y para no dirigido
+int main(int argc, char *argv[])
+{
+    int error_apertura;
+    char nombrefichero[85], opcion;
+
+	clrscr();
+    //Cargamos por defecto el fichero que se pasa como argumento del ejecutable
+	if (argc > 1)
+    {
+		cout << "Cargando datos desde el fichero dado como argumento" << endl;
+        strcpy(nombrefichero, argv[1]);
+    }
+    else
+    {
+        cout << "Introduce el nombre completo del fichero de datos" << endl;
+        cin >> nombrefichero;
+		clrscr();
+    }
+    GRAFO G(nombrefichero, error_apertura);
+    if (error_apertura == 1)
+    {
+        cout << "Error en la apertura del fichero: revisa nombre y formato" << endl;
+    }
+    else
+    {
+        do
+        {
+            menu(G.Es_dirigido(), opcion);
+            switch (opcion)
+            {
+                //Situar aquí el código de gestión de las opciones, según sea el grafo dirigido o no dirigido
+            }
+        }
+        while (opcion != 'q');
+    };
+    cout << "Fin del programa";
+	return(0);
+};
