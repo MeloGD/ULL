d7a945273c56e298fa3571ab95f1e00ffa2d85cb
SilverGG <meloglez.96@gmail.com>
2020-04-16 17:06:44 +0100

    menu, actualizar, constructor, destructor

--- Primero/Optimizacion/Pr3/FASE1-2/grafo_.cpp
+++ Primero/Optimizacion/Pr3/FASE1-2/grafo_.cpp
@@ -10,11 +10,58 @@
 
 GRAFO::~GRAFO()
 {
+    LS.clear();
+    LP.clear();
 }
 
 void GRAFO:: actualizar (char nombrefichero[85], int &errorapertura)
 {
-    
+    LS.clear();
+    LP.clear();
+
+    unsigned i, j;
+    ElementoLista aux;
+
+    cout << "Introduzca el nombre del nuevo fichero: " << endl;
+    cin >> nombrefichero;
+
+    ifstream textfile;
+    textfile.open(nombrefichero);
+
+    if (textfile.is_open())
+    {
+        textfile >> (unsigned &) n >> (unsigned &) m >> (unsigned &) dirigido;
+        
+        LS.resize(n);
+        LP.resize(n);
+        
+        for (int l = 0; l < m; l++)
+        {
+            if(dirigido == 1){
+            textfile >> (unsigned &) i >> (unsigned &) j; 
+            aux.j = j-1; //en ElementoLista, colocamos el valor del nodo-1
+            LS [i-1].push_back(aux);
+
+            aux.j = i-1;
+            LP [j-1].push_back(aux);
+            }
+            else{
+                if (i != j){ 
+                    aux.j = i-1;
+                    LS [j-1].push_back(aux);
+                }
+                else{// i=j 1-1 sería un bucle
+                }
+            }
+        }
+        textfile.close();
+        ListaPredecesores();
+
+    }
+    if (dirigido !=0 && dirigido !=1)
+    {
+        cout << "El fichero no especifica bien si es dirigido o no (0 o 1)" << endl;
+    }
 }
 
 void GRAFO::Info_Grafo()
@@ -71,6 +118,47 @@ void GRAFO::ListaPredecesores()
 
 GRAFO::GRAFO(char nombrefichero[85], int &errorapertura)
 {
+    unsigned i, j;
+    ElementoLista aux;
+
+    ifstream textfile;
+    textfile.open(nombrefichero);
+
+    if (textfile.is_open())
+    {
+        textfile >> (unsigned &) n >> (unsigned &) m >> (unsigned &) dirigido;
+        
+        LS.resize(n);
+        LP.resize(n);
+        
+        for (int l = 0; l < m; l++)
+        {
+            if(dirigido == 1){
+            textfile >> (unsigned &) i >> (unsigned &) j; 
+            aux.j = j-1; //en ElementoLista, colocamos el valor del nodo-1
+            LS [i-1].push_back(aux);
+
+            aux.j = i-1;
+            LP [j-1].push_back(aux);
+            }
+            else{
+                if (i != j){ 
+                    aux.j = i-1;
+                    LS [j-1].push_back(aux);
+                }
+                else{// i=j 1-1 sería un bucle
+                }
+            }
+        }
+        textfile.close();
+        ListaPredecesores();
+
+    }
+    if (dirigido !=0 && dirigido !=1)
+    {
+        cout << "El fichero no especifica bien si es dirigido o no (0 o 1)" << endl;
+    }
+    
 }
 
 
--- Primero/Optimizacion/Pr3/FASE1-2/pg1_.cpp
+++ Primero/Optimizacion/Pr3/FASE1-2/pg1_.cpp
@@ -9,19 +9,46 @@
 #include <string.h>
 #include "grafo.h"
 
+
 void clrscr() //No funciona en todos los sistemas: opcional
 {
-	system("cls");
+	// system("cls"); descomentar y comentar el clear si es para windows
+    system("clear");
 }
+
 
 
 void menu (unsigned dirigido, char &opcion)
 //Expresion del menu de opciones segun sea un grafo dirigido o no dirigido
 {
+
+       if(dirigido == 1){
+           cout << "\nActividad 1, Optimización: Carga del grafo y conectividad" << endl;
+           cout << "a. Cargar el grafo desde fichero" << endl;
+           cout << "b. Mostrar informacion basica del grafo" << endl;
+           cout << "c. Mostrar la lista de sucesores del grafo" << endl;
+           cout << "d. Mostrar la lista de predecesores del grafo" << endl;
+           cout << "e. Finalizar el programa" << endl;
+           cout << "Introduzca la letra de la opcion a escoger:" << endl;
+           cin >> opcion;   
+       }
+       else
+       {
+           cout << "\nActividad 1, Optimización: Carga del grafo y conectividad" << endl;
+           cout << "a. Cargar el grafo desde fichero" << endl;
+           cout << "b. Mostrar informacion basica del grafo" << endl;
+           cout << "c. Mostrar la lista de adyacencia del grafo" << endl;
+           cout << "d. Mostrar las componentes conexas del grafo" << endl;
+           cout << "e. Finalizar el programa" << endl;
+           cout << "Introduzca la letra de la opcion a escoger:" << endl;
+           cin >> opcion;   
+       }
+       
+
    
        //Cumplimentar por alumnado según pantallas de opciones en el guión u otras equivalentes
-        //prueba
-    };
+        
+};
 
 
 // El principal es simplemente un gestor de menu, diferenciando acciones para grafo dirigido y para no dirigido
@@ -34,32 +61,76 @@ int main(int argc, char *argv[])
     //Cargamos por defecto el fichero que se pasa como argumento del ejecutable
 	if (argc > 1)
     {
-		cout << "Cargando datos desde el fichero dado como argumento" << endl;
+		cout << "Cargando datos desde el fichero dado como argumento..." << endl;
         strcpy(nombrefichero, argv[1]);
     }
     else
     {
-        cout << "Introduce el nombre completo del fichero de datos" << endl;
+        cout << "Introduce el nombre completo del fichero de datos con el que quiere empezar a trabajar:" << endl;
         cin >> nombrefichero;
+        cout<< "\nHa seleccionado: " << nombrefichero << endl;
 		clrscr();
     }
     GRAFO G(nombrefichero, error_apertura);
     if (error_apertura == 1)
     {
-        cout << "Error en la apertura del fichero: revisa nombre y formato" << endl;
+        cout << "Error en la apertura del fichero: revisa nombre y formato." << endl;
     }
     else
     {
         do
         {
             menu(G.Es_dirigido(), opcion);
-            switch (opcion)
-            {
-                //Situar aquí el código de gestión de las opciones, según sea el grafo dirigido o no dirigido
-            }
-        }
-        while (opcion != 'q');
+                if (G.Es_dirigido() == 1){
+                    switch (opcion){   
+                        case 'a': G.actualizar(nombrefichero, error_apertura);
+                            while (error_apertura == 1)
+                            {
+                                G.actualizar(nombrefichero, error_apertura);
+                            }
+                            clrscr();
+                            cout<< "\nHa pasado a trabajar con: " << nombrefichero << endl;
+                            break;
+                        case 'b':
+                            clrscr();
+                            cout << "La informacion del grafo " << nombrefichero << " es:" << endl;
+                            G.Info_Grafo();
+                            break;
+                        case 'c': G.Mostrar_Listas(1);
+                            break;
+                        case 'd': G.Mostrar_Listas(0);
+                            break;
+                        case 'e': cout << "Ha escogido finalizar el programam, saludos." << endl;
+                            return (0);
+                    }
+                }    
+                else
+                {
+                    switch (opcion){
+                        case 'a': G.actualizar(nombrefichero, error_apertura);
+                            while (error_apertura == 1)
+                            {
+                                G.actualizar(nombrefichero, error_apertura);
+                            }
+                            clrscr();
+                            cout<< "\nHa pasado a trabajar con: " << nombrefichero << endl;
+                            break;
+                        case 'b':
+                            clrscr();
+                            cout << "La informacion del grafo " << nombrefichero << " es:" << endl;
+                            G.Info_Grafo();
+                            break;
+                        case 'c': G.Mostrar_Listas(1);
+                            break;
+                        case 'd': G.Mostrar_Listas(0);
+                            break;
+                        case 'e': cout << "\nHa escogido finalizar el programam saludos." << endl;
+                            return (0);
+                    }
+                }
+        }while (opcion != 'e');
     };
     cout << "Fin del programa";
 	return(0);
 };
+
