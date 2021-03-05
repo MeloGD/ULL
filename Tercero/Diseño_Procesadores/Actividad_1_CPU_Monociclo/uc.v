a55e0caae479dd2b8a79b6c55342760f3c997fd4
SilverGG <meloglez.96@gmail.com>
2021-03-05 19:24:44 +0000

    correccion casez

--- Tercero/Diseño_Procesadores/Actividad_1_CPU_Monociclo/uc.v
+++ Tercero/Diseño_Procesadores/Actividad_1_CPU_Monociclo/uc.v
@@ -3,17 +3,14 @@ module uc(input wire [5:0] opcode,
           output reg s_inc, s_inm, we3, wez, 
           output reg [2:0] op_alu);
 
-always @(opcode) begin
-  casez (opcode)
-    begin
-      6b'0000zz: s_inc = 0;
+always @(opcode)
+casez (opcode)
+    6'b000000: 
+      begin 
+        s_inc = 0;
+      end
       // 6b'0001zz: 
-      
-
-    end 
-    default: 
+    default:; 
   endcase
-  
-end
 
 endmodule
